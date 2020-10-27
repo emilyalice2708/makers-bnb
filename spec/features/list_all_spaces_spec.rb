2e15eb512c170b4eb24e3c64b0ae1129c54c674f
Meryl Rastall <merylrastall@gmail.com>
2020-10-27 15:35:22 +0000

    updated typo

--- spec/features/list_all_spaces_spec.rb
+++ spec/features/list_all_spaces_spec.rb
@@ -2,8 +2,8 @@ feature 'A user can see a list of spaces' do
   scenario ' A user go to /properties/ a can see a list of properties' do
     visit('/properties')
 
-    expect(page).to have_content 'Private room rotherhithe '
-    expect(page).to have_content 'Entire apartment in westmister '
     expect(page).to have_content 'Private room rotherhithe'
+    expect(page).to have_content 'Entire apartment in westmister'
+    expect(page).to have_content 'Private room westminster'
   end
 end
