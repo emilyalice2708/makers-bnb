47a121b7e6c68962a0292b05e550ffcaf1907e7b
Adrian.T <t.adrian88@gmail.com>
2020-10-27 15:27:07 +0000

    Added spaces page, corrected gemfile, set up database connection

--- spec/features/list_all_spaces_spec.rb
+++ spec/features/list_all_spaces_spec.rb
@@ -1,9 +1,9 @@
 feature 'A user can see a list of spaces' do
-  scenario ' A user go to /properties/ a can see a list of properties' do
-    visit('/properties')
+  scenario ' A user go to /spaces/ a can see a list of properties' do
+    visit('/spaces')
 
-    expect(page).to have_content 'Private room rotherhithe '
-    expect(page).to have_content 'Entire apartment in westmister '
+    expect(page).to have_content 'Private room rotherhithe'
+    expect(page).to have_content 'Entire apartment in westmister'
     expect(page).to have_content 'Private room rotherhithe'
   end
 end
