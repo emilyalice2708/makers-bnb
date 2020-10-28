a9e7009292d50f099897fef956ab6cf8221ebc2a
jotkafomat <k-j@xl.wp.pl>
2020-10-28 10:37:53 +0000

    setup test database

--- spec/features/list_all_spaces_spec.rb
+++ spec/features/list_all_spaces_spec.rb
@@ -1,6 +1,13 @@
+require 'pg'
+require_relative '../../lib/connection.rb'
 feature 'A user can see a list of spaces' do
   scenario ' A user go to /spaces/ a can see a list of properties' do
     visit('/spaces')
+
+    Connection.query("INSERT INTO spaces (name) VALUES ('Private room rotherhithe');")
+    Connection.query("INSERT INTO spaces (name) VALUES ('Entire apartment in westminster');")
+    Connection.query("INSERT INTO spaces (name) VALUES ('Private room westminster');")
+
 
     expect(page).to have_content 'Private room rotherhithe'
     expect(page).to have_content 'Entire apartment in westminster'
---
+++ spec/setup_test_database.rb
@@ -0,0 +1,9 @@
+require 'pg'
+require_relative '../lib/connection'
+def setup_test_database
+  p "Setting up test database"
+
+  Connection.connect('makers_bnb_manager_test')
+
+  Connection.query("TRUNCATE spaces;")
+end
