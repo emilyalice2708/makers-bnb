021677bb695c7b4a0838fb9cc35d16a88e854bc5
jotkafomat <k-j@xl.wp.pl>
2020-10-29 20:31:53 +0000

    add feature test for sign out.

--- spec/features/authentification_spec.rb
+++ spec/features/authentification_spec.rb
@@ -8,4 +8,23 @@ feature 'authentification' do
 
     expect(page).to have_content 'Welcome, Rubber Duck'
   end
+
+  scenario 'a user can sign out' do
+    user = User.create(email: 'test@example.com',
+                        password: 'password12345',
+                        display_name: 'Rubber Duck')
+
+
+    visit '/sessions/new'
+
+    fill_in(:email, with: 'test@example.com')
+    fill_in(:password, with: 'password123')
+
+    click_button('Submit')
+
+    click_button('Sign out')
+
+    expect(page).not_to have_content 'Welcome, Rubber Duck'
+  end
+
 end
